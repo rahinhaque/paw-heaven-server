076994f1e70932735dae31bf3fbedad4332bc161
Rahin Haque <haquerahin743@gmail.com>
2026-05-20 23:16:08 +0600

    Single animal delete operation

--- index.js
+++ index.js
@@ -70,6 +70,13 @@ async function run() {
      )
      res.send(result);
     });
+    //delete one animal
+    app.delete("/animals/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await animalCollection.deleteOne(query);
+      res.send(result);
+    })
 
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
